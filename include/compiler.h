8af2e5370b85f3aa6ce7470d87d54f5166d25a4c
Dinnerb0ne2 <tomma_2022@outlook.com>
2025-12-18 22:47:21 +0800

    fix(headers)

--- include/compiler.h
+++ include/compiler.h
@@ -1,6 +1,8 @@
 #ifndef COMPILER_H
 #define COMPILER_H
 
+#include <stddef.h>
+
 // Compile and execute code
 // Returns 0 on success, -1 on compilation error
 int Compiler_RunCode(const char* code, char* output, size_t output_size);
--- make.cmd
+++ make.cmd
@@ -1,14 +1,16 @@
 @echo off
+REM Build C IDE using TCC
+
 set TCC_PATH=.\tcc
 set INCLUDE_PATH=include
 set SRC_PATH=src
 
-echo Compiling
+echo Compiling...
 
 %TCC_PATH%\tcc.exe -I%INCLUDE_PATH% ^
   -o c_ide.exe ^
   %SRC_PATH%\*.c ^
-  -L%TCC_PATH%\lib ^
+  -L%TCC_PATH%\lib -I%TCC_PATH%\libtcc -I%TCC_PATH%\include\winapi ^
   -ltcc -luser32 -lkernel32 -lgdi32 -lcomctl32
 
 if %errorlevel% equ 0 (
